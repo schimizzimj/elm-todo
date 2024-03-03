961e6b217c1ae6dbb0500e57d6d8cdaa6f47ed22
schimizzimj <marcusjschimizzi@gmail.com>
2024-03-03 15:53:48 -0800

    slight refactor, add ability to mark todo as complete

--- src/Main.elm
+++ src/Main.elm
@@ -8,41 +8,80 @@ import Html.Attributes exposing (placeholder, value, type_)
 
 -- MODEL
 
+type alias Task =
+    {
+        description : String
+    ,   completed : Bool
+    ,   id : Int
+    }
+
 type alias Model =
-    { tasks: List String
-    , newTask: String
+    { tasks: List Task
+    , taskInput: String
+    , nextId: Int
     }
 
 init : Model
 init =
     { tasks = []
-    , newTask = ""
+    , taskInput = ""
+    , nextId = 0
+    }
+
+newTask : String -> Int -> Task
+newTask description id =
+    { description = description
+    , completed = False
+    , id = id
     }
 
 -- UPDATE
 
 type Msg
     = AddTask
-    | UpdateNewTask String
+    | UpdateTaskInput String
+    | CompleteTask Int
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddTask ->
-            { model | tasks = model.newTask :: model.tasks, newTask = "" }
-        UpdateNewTask newTask ->
-            { model | newTask = newTask }
+            { model | nextId = model.nextId + 1
+            ,   tasks =
+                    if String.isEmpty model.taskInput then
+                        model.tasks
+                    else
+                        (newTask model.taskInput model.nextId) :: model.tasks
+            ,   taskInput = ""
+            }
+        UpdateTaskInput newTaskDescription ->
+            { model | taskInput = newTaskDescription }
+        CompleteTask taskId ->
+            { model | tasks = List.map (completeTask taskId) model.tasks }
 
+completeTask : Int -> Task -> Task
+completeTask taskId task =
+    if task.id == taskId then
+        { task | completed = True }
+    else
+        task
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
     div []
-        [ input [ type_ "text", placeholder "Add a new task...", onInput UpdateNewTask, value model.newTask ] []
+        [ input [ type_ "text", placeholder "Add a new task...", onInput UpdateTaskInput, value model.taskInput ] []
         , button [ onClick AddTask ] [ text "Add task" ]
-        , div [] (List.map (\task -> div [] [ text task ]) model.tasks)
+        , div [] (List.map viewTask model.tasks)
         ]
+
+viewTask : Task -> Html Msg
+viewTask task =
+    div [] [ text task.description
+    , button [ onClick (CompleteTask task.id) ] [ text "Complete" ]
+    , text (if task.completed then " (completed)" else "")
+    ]
 
 -- MAIN
 
