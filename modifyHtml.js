e7b4cd901595619e6983b103308115349fa6e421
schimizzimj <marcusjschimizzi@gmail.com>
2024-03-06 10:20:20 -0800

    make sure styles are copied to output directory

--- modifyHtml.js
+++ modifyHtml.js
@@ -4,3 +4,4 @@ const scriptSrc = isProduction ? "main.min.js" : "main.js";
 const htmlTemplate = fs.readFileSync("./index.html", "utf8");
 const outputHtml = htmlTemplate.replace(/main\.js/g, scriptSrc);
 fs.writeFileSync("./dist/index.html", outputHtml);
+fs.copyFileSync("./style.css", "./dist/style.css");
