b6b05fa6cf82ee062445176ee28af8559f7178cb
schimizzimj <marcusjschimizzi@gmail.com>
2024-03-07 10:35:57 -0800

    add titles to task buttons

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,7 @@ port module Main exposing (..)
 import Browser
 import Html exposing (Html, div, text, button, input)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (placeholder, value, type_, class, style)
+import Html.Attributes exposing (placeholder, value, type_, class, style, title)
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Html.Attributes exposing (disabled)
@@ -123,8 +123,21 @@ viewTask task =
     div [
         class (if task.completed then "task completed" else "task")
     ] [ text task.description
-    , input [ type_ "checkbox", onClick (CompleteTask task.id), disabled task.completed, checked task.completed ] []
-    , button [ onClick (DeleteTask task.id), style "color" "red", style "background" "none", style "border" "none" ] [ text "X" ]
+    , input [ type_ "checkbox"
+        , onClick (CompleteTask task.id)
+        , disabled task.completed
+        , checked task.completed
+        , title "Complete task"
+        ]
+        []
+    , button [ onClick (DeleteTask task.id)
+        , style "color" "red"
+        , style "background" "none"
+        , style "border" "none"
+        , style "cursor" "pointer"
+        , title "Delete task"
+        ]
+        [ text "X" ]
     ]
 
 viewControls : Model -> Html Msg
